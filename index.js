1fdac1e4b7d36e4f08b49ed6eabf34d7d701d30c
pantzer20 <pantzlaff@wisc.edu>
2018-12-03 20:37:59 -0500

    Use jquery more, migrate to TopoJSON

--- index.js
+++ index.js
@@ -19,140 +19,18 @@ var districtsSearch = new Fuse(districtsList, {
     keys: ['a', 's', 'n']
 });
 
-for (var i = 0; i < 5; i++) {
-    window['result' + i] = document.getElementById('result' + i);
-} 
-
-mapboxgl.accessToken = 'pk.eyJ1IjoicGFudHplciIsImEiOiJjaXNna2wxbm0wMXc2MnludmJxc3QxanE5In0.z4TQn08v14lHWSgDJnWcDQ';
-
-var map = new mapboxgl.Map({
-    container: 'map',
-    minZoom: 4,
-    maxZoom: 11,
-    style: 'mapbox://styles/mapbox/streets-v9',
-    hash: true,
-    attributionControl: false
-});
-
-function headerHeight(padding) {
+function headerHeight(padding=0) {
     return document.querySelector('header').clientHeight + padding;
 }
-function footerHeight(padding) {
+function footerHeight(padding=0) {
     return document.querySelector('footer').clientHeight + padding;
 }
 
-map.fitBounds([[-128, 25], [-65, 50]], {
-    padding: {
-        top: headerHeight(25),
-        bottom: footerHeight(15),
-        left: 0,
-        right: 0
-    }
-});
-
-map.on('load', function() {
-    map.addSource('states', {
-        'type': 'vector',
-        'url': 'mapbox://pantzer.avtg8bc5'
-    });
-    map.addSource('districts', {
-        'type': 'vector',
-        'url': 'mapbox://pantzer.btv1xx5n'
-    });
-    map.addLayer({
-        'id': 'electoral',
-        'type': 'fill',
-        'source': 'states',
-        'source-layer': 'states-5es6xp',
-        'layout': {
-            visibility: 'visible'
-        },
-        'paint': {
-            'fill-opacity': 0.55,
-            'fill-color': {
-                'property': 'persons_el',
-                'stops': [[187875, '#f1a340'], [581499, '#ebdddd'], [677344, '#998ec3']]
-            }
-        }
-    });
-    map.addLayer({
-        'id': 'house',
-        'type': 'fill',
-        'source': 'districts',
-        'source-layer': 'districts-7hbquq',
-        'layout': {
-            visibility: 'none'
-        },
-        'paint': {
-            'fill-opacity': 0.47,
-            'fill-color': {
-                'property': 'population',
-                'stops': [[526284, '#f1a340'], [719187, '#ebdddd'], [989414, '#998ec3']]
-            }
-        }
-    });
-    map.addLayer({
-        'id': 'house-outline',
-        'type': 'line',
-        'source': 'districts',
-        'source-layer': 'districts-7hbquq',
-        'layout': {
-            visibility: 'none'
-        },
-        'paint': {
-            'line-opacity': {'stops': [[4, 0.15], [11, 0.5]]},
-            'line-color': '#444444',
-            'line-width': {'stops': [[4, 0.3], [11, 1]]}
-        }
-    });
-    map.addLayer({
-        'id': 'senate',
-        'type': 'fill',
-        'source': 'states',
-        'source-layer': 'states-5es6xp',
-        'layout': {
-            visibility: 'none'
-        },
-        'paint': {
-            'fill-opacity': 0.47,
-            'fill-color': {
-                'property': 'persons_se',
-                'stops': [[281813, '#f1a340'], [3128465, '#ebdddd'], [18626978, '#998ec3']]
-            }
-        },
-        'filter' : ['!=', 'geoid', '11']
-    });
-});
-
-map.on('click', 'electoral', function(e) {
-    if (map.getLayoutProperty('electoral', 'visibility') === 'visible') {
-        updateQueryInfo('electoral', e.features[0].properties.name);
-    }
-});
-map.on('mouseenter', 'electoral', () => map.getCanvas().style.cursor = 'pointer');
-map.on('mouseleave', 'electoral', () => map.getCanvas().style.cursor = '');
-
-map.on('click', 'house', function(e) {
-    if (map.getLayoutProperty('house', 'visibility') === 'visible') {
-        updateQueryInfo('house', e.features[0].properties.abbreviati);
-    }
-});
-map.on('mouseenter', 'house', () => map.getCanvas().style.cursor = 'pointer');
-map.on('mouseleave', 'house', () => map.getCanvas().style.cursor = '');
-
-map.on('click', 'senate', function(e) {
-    if (map.getLayoutProperty('senate', 'visibility') === 'visible') {
-        updateQueryInfo('senate', e.features[0].properties.name);
-    }
-});
-map.on('mouseenter', 'senate', () => map.getCanvas().style.cursor = 'pointer');
-map.on('mouseleave', 'senate', () => map.getCanvas().style.cursor = '');
-
 function updateLegend(high, mid, low, text) {
-    document.getElementById('highVal').innerHTML = high;
-    document.getElementById('midVal').innerHTML = mid;
-    document.getElementById('lowVal').innerHTML = low;
-    document.getElementById('legendTitle').innerHTML = 'Representation strength <small>(persons/' + text + ')</small>';
+    $('#highVal').html(high);
+    $('#midVal').html(mid);
+    $('#lowVal').html(low);
+    $('legendTitle').html('Representation strength <small>(persons/' + text + ')</small>');
 }
 
 function toggleLayer(layer, feature) {
@@ -184,8 +62,8 @@ function toggleLayer(layer, feature) {
                 right: 0
             }
         });
-        document.getElementById('searchInput').value = '';
-        document.getElementById('layerSelect').value = layer;
+        $('#searchInput').val('');
+        $('#layerSelect').val(layer);
     }
 }
 
@@ -193,17 +71,17 @@ function updateQueryInfo(layer, feature) {
     if (layer === 'electoral') {
         var queried = map.queryRenderedFeatures({filter: ['==', 'name', feature]});
         var props = queried[0].properties;
-        var powerDec = 581499 / props.persons_el;
+        var powerDec = 581499 / props.persons_elector;
         if (powerDec >= 1) {
             var powerPct = (Math.round((powerDec - 1) * 100)).toString() + '% more';
         } else {
             var powerPct = (Math.round(Math.abs(powerDec - 1) * 100)).toString() + '% less';
         }
-        document.getElementById('title').innerHTML = props.name;
-        document.getElementById('narrative').innerHTML = 'Home to ' + props.population.toLocaleString() + ' residents, ' + props.name + ' is able to send ' +  props.electors.toLocaleString() + ' presidential electors to represent it in the Electoral College. This provides it one elector for every ' + props.persons_el.toLocaleString() + ' residents. When compared to the national average of one elector per 581,499 people, a resident of ' + props.name + ' has <b>' + powerPct + ' power</b> to choose the president than the average American.';
-        document.getElementById('queryInfo').style.display = 'block';
+        $('#title').html(props.name);
+        $('#narrative').html('Home to ' + props.population.toLocaleString() + ' residents, ' + props.name + ' is able to send ' +  props.electors.toLocaleString() + ' presidential electors to represent it in the Electoral College. This provides it one elector for every ' + props.persons_elector.toLocaleString() + ' residents. When compared to the national average of one elector per 581,499 people, a resident of ' + props.name + ' has <b>' + powerPct + ' power</b> to choose the president than the average American.');
+        $('#queryInfo').css('display', 'block');
     } else if (layer === 'house') {
-        var queried = map.queryRenderedFeatures({filter: ['==', 'abbreviati', feature]});
+        var queried = map.queryRenderedFeatures({filter: ['==', 'abbreviation', feature]});
         var props = queried[0].properties;
         var powerDec = 719187 / props.population;
         if (powerDec >= 1) {
@@ -211,21 +89,21 @@ function updateQueryInfo(layer, feature) {
         } else {
             var powerPct = (Math.round(Math.abs(powerDec - 1) * 100)).toString() + '% less';
         }
-        document.getElementById('title').innerHTML = props.abbreviati;
-        document.getElementById('narrative').innerHTML = props.state + ' ' + props.name + ' is home to ' + props.population.toLocaleString() + ' residents. When compared to the national average of one representative per 719,187 people, a resident of this district has <b>' + powerPct + ' power</b> to choose a representative than the average American.';
-        document.getElementById('queryInfo').style.display = 'block';
+        $('#title').html(props.abbreviation);
+        $('#narrative').html(props.state + ' ' + props.name + ' is home to ' + props.population.toLocaleString() + ' residents. When compared to the national average of one representative per 719,187 people, a resident of this district has <b>' + powerPct + ' power</b> to choose a representative than the average American.');
+        $('#queryInfo').css('display', 'block');
     } else if (layer === 'senate') {
         var queried = map.queryRenderedFeatures({filter: ['==', 'name', feature]});
         var props = queried[0].properties;
-        var powerDec = 3128465 / props.persons_se;
+        var powerDec = 3128465 / props.persons_sen;
         if (powerDec >= 1) {
             var powerPct = (Math.round((powerDec - 1) * 100)).toString() + '% more';
         } else {
             var powerPct = (Math.round(Math.abs(powerDec - 1) * 100)).toString() + '% less';
         }
-        document.getElementById('title').innerHTML = props.name;
-        document.getElementById('narrative').innerHTML = 'Home to ' + props.population.toLocaleString() + ' residents, ' + props.name + ' has one U.S. senator for every ' + props.persons_se.toLocaleString() + ' of its inhabitants. When compared to the national average of one senator per 3,128,465 people, a resident of ' + props.name + ' has <b>' + powerPct + ' power</b> to choose a senator than the average American.';
-        document.getElementById('queryInfo').style.display = 'block';
+        $('#title').html(props.name);
+        $('#narrative').html('Home to ' + props.population.toLocaleString() + ' residents, ' + props.name + ' has one U.S. senator for every ' + props.persons_sen.toLocaleString() + ' of its inhabitants. When compared to the national average of one senator per 3,128,465 people, a resident of ' + props.name + ' has <b>' + powerPct + ' power</b> to choose a senator than the average American.');
+        $('#queryInfo').css('display', 'block');
     }
 }
 
@@ -237,32 +115,33 @@ function geocode(input) {
             if (!map.isMoving() && data.candidates) {
                 var candidate = data.candidates[0];
                 try {
-                    result0.innerHTML =  '<b>Place</b>: ' + candidate.address;
-                    result0.style.display = 'block';
-                    result0.onclick = function() {
-                        clearResults();
-                        map.flyTo({
-                            center: [candidate.location.x, candidate.location.y],
-                            zoom: 11
-                        });
-                        document.getElementById('searchInput').value = '';
-                    };
+                    $('#result0')
+                      .html('<b>Place</b>: ' + candidate.address)
+                      .css('display', 'block')
+                      .on('click', function() {
+                          clearResults();
+                          map.flyTo({
+                              center: [candidate.location.x, candidate.location.y],
+                              zoom: 11
+                          });
+                          $('#searchInput').val('');
+                      });
                 } catch(err) {
-                    result0.style.display = 'none';
-                    result0.onclick = null;
+                    $('#result0')
+                      .css('display', 'none')
+                      .off('click');
                 }
             } else {
-                result0.style.display = 'none';
-                result0.onclick = null;
+                $('#result0')
+                  .css('display', 'none')
+                  .off('click');
             }
         }
     });
 }
 
 function clearResults() {
-    for (var i = 0; i < 5; i++) {
-        window['result' + i].style.display = 'none';
-    }
+    $('.result').css('display', 'none');
 }
 
 function search(input) {
@@ -274,42 +153,157 @@ function search(input) {
         var stateResults = statesSearch.search(inputText);
         var countStateResults = stateResults.length;
         if (countStateResults > 0) {
-            result1.innerHTML = '<b>State</b>: ' + stateResults[0].n;
-            result1.style.display = 'block';
-            result1.onclick = function() {
-                document.getElementById('queryInfo').style.display = 'none';
-                toggleLayer('electoral', stateResults[0]);
-                clearResults();
-            }
+            $('#result1')
+              .html('<b>State</b>: ' + stateResults[0].n)
+              .css('display', 'block')
+              .on('click', function() {
+                  $('#queryInfo').css('display', 'none');
+                  toggleLayer('electoral', stateResults[0]);
+                  clearResults();
+              });
             if (countStateResults > 1) {
-                result2.innerHTML = '<b>State</b>: ' + stateResults[1].n;
-                result2.style.display = 'block';
-                result2.onclick = function() {
-                    document.getElementById('queryInfo').style.display = 'none';
-                    toggleLayer('electoral', stateResults[1]);
-                    clearResults();
-                }
+                $('#result2')
+                  .html('<b>State</b>: ' + stateResults[1].n)
+                  .css('display', 'block')
+                  .on('click', function() {
+                      $('#queryInfo').css('display', 'none');
+                      toggleLayer('electoral', stateResults[1]);
+                      clearResults();
+                  });
             }
         }
         var districtResults = districtsSearch.search(inputText);
         var countDistrictResults = districtResults.length;
         if (countDistrictResults > 0) {
-            result3.innerHTML = '<b>District</b>: ' + districtResults[0].a;
-            result3.style.display = 'block';
-            result3.onclick = function() {
-                document.getElementById('queryInfo').style.display = 'none';
-                toggleLayer('house', districtResults[0]);
-                clearResults();
-            }
+            $('#result3')
+              .html('<b>District</b>: ' + districtResults[0].a)
+              .css('display', 'block')
+              .on('click', function() {
+                  $('#queryInfo').css('display', 'none');
+                  toggleLayer('house', districtResults[0]);
+                  clearResults();
+              });
             if (countDistrictResults > 1) {
-                result4.innerHTML = '<b>District</b>: ' + districtResults[1].a;
-                result4.style.display = 'block';
-                result4.onclick = function() {
-                    document.getElementById('queryInfo').style.display = 'none';
-                    toggleLayer('house', districtResults[1]);
-                    clearResults();
-                }
+                $('#result4')
+                  .html('<b>District</b>: ' + districtResults[1].a)
+                  .css('display', 'block')
+                  .on('click', function() {
+                      $('#queryInfo').css('display', 'none');
+                      toggleLayer('house', districtResults[1]);
+                      clearResults();
+                  });
             }
         }
     }
 }
+
+mapboxgl.accessToken = 'pk.eyJ1IjoicGFudHplciIsImEiOiJjaXNna2wxbm0wMXc2MnludmJxc3QxanE5In0.z4TQn08v14lHWSgDJnWcDQ';
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    minZoom: 1.5,
+    maxZoom: 11,
+    style: 'mapbox://styles/mapbox/streets-v9',
+    hash: true,
+    attributionControl: false
+});
+
+map.fitBounds([[-128, 25], [-65, 50]], {
+    padding: {
+        top: headerHeight(25),
+        bottom: footerHeight(15),
+        left: 0,
+        right: 0
+    }
+});
+
+map.on('load', function() {
+    $.get({
+        url: 'features.topojson',
+        dataType: 'json',
+        success: function(d) {
+            statesData = topojson.feature(d, d.objects.states);
+            districtsData = topojson.feature(d, d.objects.districts);
+            map.addSource('states', {
+                'type': 'geojson',
+                'data': statesData
+            });
+            map.addSource('districts', {
+                'type': 'geojson',
+                'data': districtsData
+            });
+            map.addLayer({
+                'id': 'electoral',
+                'type': 'fill',
+                'source': 'states',
+                'layout': {
+                    visibility: 'visible'
+                },
+                'paint': {
+                    'fill-opacity': 0.55,
+                    'fill-color': {
+                        'property': 'persons_elector',
+                        'stops': [[187875, '#f1a340'], [581499, '#ebdddd'], [677344, '#998ec3']]
+                    }
+                }
+            });
+            map.addLayer({
+                'id': 'house',
+                'type': 'fill',
+                'source': 'districts',
+                'layout': {
+                    visibility: 'none'
+                },
+                'paint': {
+                    'fill-opacity': 0.47,
+                    'fill-color': {
+                        'property': 'population',
+                        'stops': [[526284, '#f1a340'], [719187, '#ebdddd'], [989414, '#998ec3']]
+                    }
+                }
+            });
+            map.addLayer({
+                'id': 'house-outline',
+                'type': 'line',
+                'source': 'districts',
+                'layout': {
+                    visibility: 'none'
+                },
+                'paint': {
+                    'line-opacity': {'stops': [[4, 0.15], [11, 0.5]]},
+                    'line-color': '#444444',
+                    'line-width': {'stops': [[4, 0.3], [11, 1]]}
+                }
+            });
+            map.addLayer({
+                'id': 'senate',
+                'type': 'fill',
+                'source': 'states',
+                'layout': {
+                    visibility: 'none'
+                },
+                'paint': {
+                    'fill-opacity': 0.47,
+                    'fill-color': {
+                        'property': 'persons_sen',
+                        'stops': [[281813, '#f1a340'], [3128465, '#ebdddd'], [18626978, '#998ec3']]
+                    }
+                },
+                'filter' : ['!=', 'geoid', '11']
+            });
+        }
+    });
+    ['electoral', 'house', 'senate'].forEach(function(layer) {
+        map.on('click', layer, function(e) {
+            if (map.getLayoutProperty(layer, 'visibility') === 'visible') {
+                updateQueryInfo(layer, (layer === 'house' ? e.features[0].properties.abbreviation : e.features[0].properties.name));
+            }
+        });
+        map.on('mouseenter', layer, function() {
+            map.getCanvas().style.cursor = 'pointer';
+        });
+        map.on('mouseleave', 'electoral', function() {
+            map.getCanvas().style.cursor = '';
+        });
+    });
+});
